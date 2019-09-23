8bd3a6ad414c45e03dcb3f6533b349907243d636
Dan Fitz <dan@danielfitz.com>
2019-09-23 18:40:47 -0400

    Added feature where element is only in focus when in middle of viewport

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,50 @@
+const portfolio = {};
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+$.fn.isInViewport = function() {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
+
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+
+  return elementBottom > viewportTop + ((viewportBottom - viewportTop) * 0.40) && elementTop < viewportBottom - ((viewportBottom - viewportTop) * 0.40);
+};
+
+portfolio.init = function() {
+  AOS.init();
+
+  $(window).on('resize scroll', function () {
+    $(".focusable").each(function() {
+      if ($(this).isInViewport()) {
+        console.log("HELLO");
+        $(this).css({
+          "opacity": "1",
+          bottom: 0
+        });
+      } else {
+        $(this).css({
+          "opacity": "0.2",
+          bottom: "-0.8rem"
+        });
+      };
+    });
+  });
+};
+
 $(document).ready(function() {
-  console.log("HELLO");
+  portfolio.init();
 });
