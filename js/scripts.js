69d367a2773a70c6190e142483af0199dad83692
Dan Fitz <dan@danielfitz.com>
2019-09-24 11:37:03 -0400

    Updated descriptions of projects and linked to assets + styled anchor links

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,5 @@
 const portfolio = {};
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener('resize', () => {
-
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 $.fn.isInViewport = function() {
   var elementTop = $(this).offset().top;
   var elementBottom = elementTop + $(this).outerHeight();
@@ -25,20 +11,16 @@ $.fn.isInViewport = function() {
 };
 
 portfolio.init = function() {
-  AOS.init();
-
-  $(window).on('resize scroll', function () {
+  $(window).on("resize scroll load", function () {
     $(".focusable").each(function() {
       if ($(this).isInViewport()) {
-        $(this).css({
-          opacity: 1,
-          bottom: 0
-        });
+        $(this)
+          .addClass("focused")
+          .removeClass("unfocused");
       } else {
-        $(this).css({
-          opacity: 0.3,
-          bottom: "-0.8rem"
-        });
+        $(this)
+          .addClass("unfocused")
+          .removeClass("focused");
       };
     });
   });
